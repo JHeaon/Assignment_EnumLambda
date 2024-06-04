619d5297b0694d4807b85b225e28ac68234eb0a7
JHeaon <j3heawon@gmail.com>
2024-06-04 23:22:32 +0900

    ♻️ refactor: Enum, Lambda을 이용하여 코드 리팩토링

--- src/sparta/service/Coupon.java
+++ src/sparta/service/Coupon.java
@@ -11,18 +11,7 @@ public class Coupon {
         this.couponPrice = couponPrice;
     }
 
-    public int calcPrice(DiscountEvent event) {
-        int benefit;
-
-        switch (event) {
-            case NONE -> benefit = couponPrice;
-            case SUMMER -> benefit = (int) (couponPrice * 0.1);
-            case WINTER -> benefit = (int) (couponPrice * 0.2);
-            case BLACK_FRIDAY -> benefit = (int) (couponPrice * 0.3);
-            case NEW_YEAR -> benefit = (int) (couponPrice * 0.5);
-            default -> benefit = 0;
-        }
-
-        return couponPrice + benefit;
+    public int calcPrice(DiscountEvent event, int price) {
+        return price + event.clac(price);
     }
 }
